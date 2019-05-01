ad3f47c0b924b5fc5ea8674e4f2bf467922a8411
palm <m.zero13@gmail.com>
2019-05-01 11:34:22 +0700

    Register Success

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -89,7 +89,7 @@ class _RegisterState extends State<Register> {
     );
   }
 
-  Widget uploadButton() {
+  Widget uploadButton(BuildContext context) {
     return IconButton(
       icon: Icon(Icons.cloud_upload),
       tooltip: 'Upload To Firebase',
@@ -99,18 +99,19 @@ class _RegisterState extends State<Register> {
           formKey.currentState.save();
           print(
               'name = $nameString, email = $emailString, password = $passwordString');
-          uploadValueToFirebase();
+          uploadValueToFirebase(context);
         }
       },
     );
   }
 
-  void uploadValueToFirebase() async {
+  void uploadValueToFirebase(BuildContext context) async {
     FirebaseUser firebaseUser = await firebaseAuth
         .createUserWithEmailAndPassword(
             email: emailString, password: passwordString)
         .then((user) {
       print('Register Success With ===>>> $user');
+      Navigator.pop(context);
     }).catchError((error) {
       String errorString = error.message;
       print('HAVE ERROR ===>>> $errorString');
@@ -139,7 +140,7 @@ class _RegisterState extends State<Register> {
         appBar: AppBar(
           backgroundColor: Colors.orange[900],
           title: Text('Register'),
-          actions: <Widget>[uploadButton()],
+          actions: <Widget>[uploadButton(context)],
         ),
         body: Form(
           key: formKey,
